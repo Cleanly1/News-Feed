64ea5cb12a6e804409e5dc39e5f84a3de9ad6745
Cleanly <oskar.turesson99@gmail.com>
2019-10-23 10:14:39 +0200

    added the ds_store file

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
-  <title>Your title</title>
+  <title>Plain News</title>
 </head>
 <body>
-  
+
   <script type="text/javascript" src="script.js">
 
   </script>
